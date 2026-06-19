71c8621ca602efc0294c9750d1ac004e7f14eb50
Tom Lausmann <tom.lausmann@justeattakeaway.com>
2026-06-19 18:03:39 +0200

    feat: no title bar for participant view

---
+++ app/admin/layout.tsx
@@ -0,0 +1,14 @@
+import Header from "../components/organisms/Header";
+
+export default function AdminLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  );
+}
